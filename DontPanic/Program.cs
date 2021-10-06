55baa7e583d27f2a1870150f695ecb350c3c6a56
Opilane TTHK <opilane@tthk.ee>
2021-10-06 11:23:07 +0300

    1

---
+++ DontPanic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DontPanic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dontPanic = "Don't panic".ToUpper();
+            for(int i = dontPanic.Length-1; i >=0; i--)
+            { 
+               if (dontPanic[i] == 'n')
+            }
+
+            {
+                counter++;
+            }
+              Console.WriteLine($"Lauses {dontPanic} on  {counter} 'n'
+
+
+        }
+    }
+}
---
+++ HOLCounter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HOCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kuvab mitu 'h', 'o', ja 'l' tähte on lauses "Hello, World!"
+
+            string helloW = "Hello World!".ToLower();
+
+            int counter1 = 0;
+            int counter = 0;
+            int counter2 = 0;
+            for (int i = 0; i < helloW.Length; i++)
+
+            {
+                if (helloW[i] == 'h')
+                {
+                    counter2++;
+                }
+                else if (helloW[i] == 'o')
+                {
+                    counter1++;
+                }
+                else if(helloW[i] == 'l')
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine($"lauses {helloW} on {counter2} 'h' {counter1} 'o' ja {counter} 'l' tähte");
+        }
+    }
+}
